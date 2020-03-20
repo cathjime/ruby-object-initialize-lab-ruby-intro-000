047e090eec6080bd071173bf7301fcefd8070264
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-03-20 18:21:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
@@ -17,6 +17,6 @@ class Dog
   end
 
   def breed
-    @breed 
+    @breed
   end
 end
